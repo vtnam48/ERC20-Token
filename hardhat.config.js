b9dcc89cdaddfd3af6beffa0caaff3643782af76
namvt48 <vuthenam.9.3.4@gmail.com>
2024-04-12 09:33:09 +0700

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,22 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.21",
+  networks: {
+    testnet: {
+      // url: "https://data-seed-prebsc-1-s1.bnbchain.org:8545",
+      url: "https://sepolia.infura.io/v3/",
+
+      // chainId: 97,
+      chainId: 11155111,
+
+      // gasPrice: 20000000000,
+
+      // accounts: [`0x${process.env.PRIV_KEY}`]
+      accounts: [process.env.PRIV_KEY]
+
+    },
+  }
 };
